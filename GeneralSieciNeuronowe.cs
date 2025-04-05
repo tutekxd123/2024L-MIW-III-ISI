cee9714c9e524a2c4ed6d8ca8b54a39e9d32a714
Piotr Marzec <52503806+tutekxd123@users.noreply.github.com>
2025-04-05 18:49:25 +0200

    Update GeneralSieciNeuronowe.cs

--- GeneralSieciNeuronowe.cs
+++ GeneralSieciNeuronowe.cs
@@ -7,6 +7,8 @@ class Neuron
 {
     public List<double> wagi = new List<double>();
     public double value = 0;
+    public double extranumber = 0;
+    public double error = 0;
     public Neuron(List<double> wagi)
     {
         this.wagi = wagi;
@@ -19,11 +21,16 @@ class Neuron
             wagi.Add(RandomNumber.rnd.NextDouble());
         }
     }
+    public double Sigmoid()
+    {
+        return 1.0 / (1 + Math.Exp(-this.value));
+    }
+
     
 }
 class Warstwa
 {
-    List<Neuron>Neurons = new List<Neuron>();
+    public List<Neuron>Neurons = new List<Neuron>();
     public Warstwa(List<Neuron> neurons)
     {
         Neurons = neurons;
@@ -49,6 +56,7 @@ class Warstwa
             {
                 this.Neurons[i].value += preview.Neurons[j].value * this.Neurons[i].wagi[j];
             }
+            this.Neurons[i].value = this.Neurons[i].Sigmoid(); //Sigmoid aktywacja?
         }
     }
     public void changeValue(List<double> Neuronvalue)
@@ -84,13 +92,62 @@ class Network
             Warstwa[i].CalcValue(Warstwa[i - 1]);
         }
     }
+    public List<double> getResultLast()
+    {
+        List<double> result = new List<double>();
+        foreach(Neuron neuron in Warstwa[Warstwa.Count - 1].Neurons)
+        {
+            result.Add(neuron.value);
+        }
+        return result;
+    }
+    public List<List<double>> CheckError(List<List<double>> probki)
+    {
+        List<List<double>> result = new List<List<double>>();
+        var lastWarstwa = this.Warstwa[this.Warstwa.Count - 1];
+        if (probki[0].Count!=(this.Warstwa[0].Neurons.Count + lastWarstwa.Neurons.Count) )
+        {
+            throw new Exception("Próbka ma nieprawidlowy rozmiar probek");
+        }
+        for(int i=0;i<probki.Count;i++)
+
+        {
+            //mamy listy
+            List<double> input = probki[i].Slice(0, this.Warstwa[0].Neurons.Count);
+            List<double> output = probki[i].Slice(this.Warstwa[0].Neurons.Count, lastWarstwa.Neurons.Count);
+            this.calcValue(input);
+            List<double> resultlast = this.getResultLast();
+            //Co dalej?
+            result.Add(new List<double>());
+            Console.WriteLine("Dla próbki {0}", string.Join(",", probki[i]));
+            for (int j=0;j<resultlast.Count;j++){
+                double tempresult = Math.Pow(resultlast[j] - output[j], 2);
+                Console.WriteLine("Neuron nr.{0} Bład:{1}",j,tempresult);
+                lastWarstwa.Neurons[j].error = tempresult;
+                result[i].Add(tempresult);
+            }
+            
+            
+        }
+        
+        return result;
+    }
 }
 class Program
 {
     static void Main()
     {
+        List<List<double>> probki = new List<List<double>>()
+        {
+            new List<double>{0,0,0},
+            new List<double>{1,0,1},
+            new List<double>{0,1,1},
+            new List<double>{1,1,0}
+        };
+
         Network test = new Network(new List<int> { 2, 1});
-        test.calcValue(new List<double> { 0,1 });
-        Console.WriteLine("Test");
+        //test.calcValue(new List<double> { 0,1 });
+        test.CheckError(probki);
+
     }
 }
